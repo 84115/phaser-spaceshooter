7e5f670208255ded55aab328f9c47569648a4212
James <jamesballsemail@gmail.com>
2018-08-08 14:15:15 +0100

    Added Boilerplate Group: Spikes

---
+++ src/groups/Spikes.js
@@ -0,0 +1,15 @@
+import Group from '../groups/Group';
+import Enemy from '../sprites/Enemy';
+import Bullet from '../sprites/Bullet';
+
+class SpikeGroup extends Group
+{
+
+	constructor(scene)
+	{
+		super(scene);
+	}
+
+}
+
+export default SpikeGroup;
