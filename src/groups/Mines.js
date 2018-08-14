d05244b28e0976c0b2ae24a77128e79f77009c9a
James Ball <hello@james-ball.co.uk>
2018-08-14 13:21:56 +0100

    Moved Mine Timer Into Function; animate()

--- src/groups/Mines.js
+++ src/groups/Mines.js
@@ -11,72 +11,26 @@ class MinesGroup extends Group
 		this.prev = [];
 		this.count = 0;
 		this.sequenceDone = false;
+		this.key = key;
+		this.tint = tint;
+		this.limit = limit;
+		this.interval = interval;
+		this.disableWeapon = disableWeapon;
 
 		if (this.scene.background.scroll)
 		{
 			this.scene.background.scroll = this.scene.background.scrollBase * 12;
 		}
 
-		if (disableWeapon)
+		if (this.disableWeapon)
 		{
 			this.scene.ship.bullets.enabled = false;
 		}
 
 		this.timer = this.scene.time.addEvent({
-			delay: interval,
-			callback: () =>
-			{
-				var pos = this.scene.grid.randomX();
-
-				for (var i = 0; i <= 11; i++)
-				{
-					if (this.prev.includes(pos))
-					{
-						var pos = this.scene.grid.randomX();
-					}
-				}
-
-				this.prev.unshift(pos);
-				this.prev = this.prev.slice(0, 11);
-
-				let mine = new Enemy(this.scene, pos, this.scene.grid[0], key);
-				mine.speed = Phaser.Math.GetSpeed(Phaser.Math.RND.between(175, 225), 1);
-				mine.maxHealth = 1;
-				mine.health = mine.maxHealth;
-
-				if (tint)
-				{
-					mine.setTint(mine);
-				}
-
-				this.add(mine);
-
-				this.count++;
-
-				if (this.count >= limit)
-				{
-					this.timer.remove(false);
-					this.prev = [];
-					this.count = 0;
-
-					this.scene.time.addEvent({
-						delay: 4000,
-						callback: () =>
-						{
-							this.scene.background.scroll = this.scene.background.scrollBase;
-
-							if (disableWeapon)
-							{
-								this.scene.ship.bullets.enabled = true;
-							}
-
-							this.clear();
-
-							this.sequenceDone = true;
-						}
-					});
-				}
-			},
+			delay: this.interval,
+			callback: this.animate,
+			callbackScope: this,
 			loop: true
 		});
 	}
@@ -113,6 +67,60 @@ class MinesGroup extends Group
 		else
 		{
 			return false;
+		}
+	}
+
+	animate()
+	{
+		var pos = this.scene.grid.randomX();
+
+		for (var i = 0; i <= 11; i++)
+		{
+			if (this.prev.includes(pos))
+			{
+				var pos = this.scene.grid.randomX();
+			}
+		}
+
+		this.prev.unshift(pos);
+		this.prev = this.prev.slice(0, 11);
+
+		let mine = new Enemy(this.scene, pos, this.scene.grid[0], this.key);
+		mine.speed = Phaser.Math.GetSpeed(Phaser.Math.RND.between(175, 225), 1);
+		mine.maxHealth = 1;
+		mine.health = mine.maxHealth;
+
+		if (this.tint)
+		{
+			mine.setTint(mine);
+		}
+
+		this.add(mine);
+
+		this.count++;
+
+		if (this.count >= this.limit)
+		{
+			this.timer.remove(false);
+			this.prev = [];
+			this.count = 0;
+
+			this.scene.time.addEvent({
+				delay: 4000,
+				callback: () =>
+				{
+					this.scene.background.scroll = this.scene.background.scrollBase;
+
+					if (this.disableWeapon)
+					{
+						this.scene.ship.bullets.enabled = true;
+					}
+
+					this.clear();
+
+					this.sequenceDone = true;
+				}
+			});
 		}
 	}
 
