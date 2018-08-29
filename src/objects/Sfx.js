6c4fd8d081eba69132643e8237fd1c89630cb9aa
James Ball <hello@james-ball.co.uk>
2018-08-29 18:58:14 +0100

    Converted Sfx to a class

--- src/objects/Sfx.js
+++ src/objects/Sfx.js
@@ -1,25 +1,41 @@
-function SfxObject(scene)
+class Sfx
 {
-	let markers = [
-		{ "name": "alien death", "start": 1, "duration": 1 },
-		{ "name": "boss hit", "start": 3, "duration": 0.5 },
-		{ "name": "escape", "start": 4, "duration": 3.2 },
-		{ "name": "meow", "start": 8, "duration": 0.5 },
-		{ "name": "numkey", "start": 9, "duration": 0.1 },
-		{ "name": "ping", "start": 10, "duration": 1 },
-		{ "name": "death", "start": 12, "duration": 4.2 },
-		{ "name": "shot", "start": 17, "duration": 1 },
-		{ "name": "squit", "start": 19, "duration": 0.3 },
-	]
 
-	let sfx = scene.sound.add('sfx');
-
-	for (let i = markers.length - 1; i >= 0; i--)
+	constructor(scene)
 	{
-		sfx.addMarker(markers[i]);
+		this.scene = scene;
+
+		this.markers = [
+			{ "name": "alien death", "start": 1, "duration": 1 },
+			{ "name": "boss hit", "start": 3, "duration": 0.5 },
+			{ "name": "escape", "start": 4, "duration": 3.2 },
+			{ "name": "meow", "start": 8, "duration": 0.5 },
+			{ "name": "numkey", "start": 9, "duration": 0.1 },
+			{ "name": "ping", "start": 10, "duration": 1 },
+			{ "name": "death", "start": 12, "duration": 4.2 },
+			{ "name": "shot", "start": 17, "duration": 1 },
+			{ "name": "squit", "start": 19, "duration": 0.3 },
+		]
+
+		this.audio = this.scene.sound.add('sfx');
+
+		for (let i = this.markers.length - 1; i >= 0; i--)
+		{
+			this.audio.addMarker(this.markers[i]);
+		}
+
+		return this;
 	}
 
-	return sfx;
+	play(key="")
+	{
+		console.log(key);
+		if (key)
+		{
+			this.audio.play(key);
+		}
+	}
+
 }
 
-export default SfxObject;
+export default Sfx;
