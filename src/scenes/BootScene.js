72b34660bf42995e577bd749df2b17310c671d2e
James <jamesballsemail@gmail.com>
2018-08-29 08:57:20 +0100

    Preload Cleanup

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -8,20 +8,43 @@ class BootScene extends Phaser.Scene
 
 	preload()
 	{
+		this.progress = this.add.graphics();
+
+		this
+			.preloadImages()
+			.preloadFonts()
+			.preloadSpritesheets()
+			.preloadAudio();
+
+		this.load.on('progress', this.onProgress, this);
+
+		this.load.on('complete', this.onComplete, this);
+	}
+
+	preloadImages()
+	{
+		// Player
 		this.load.image('ship', 'assets/sprites/shmup-ship.png');
 
+		// Background
 		this.load.image('sky', 'assets/sprites/space.png');
 
-		this.load.image('bullet', 'assets/sprites/bullet.png');
-		this.load.image('bullet-2', 'assets/sprites/enemy-bullet.png');
-
+		// Enemies
 		this.load.image('mine', 'assets/sprites/mine.png');
 		this.load.image('ufo', 'assets/sprites/ufo.png');
 		this.load.image('alien', 'assets/sprites/space-baddie.png');
 		this.load.image('brain', 'assets/sprites/brain.png');
+		// this.load.image('asteroids', 'assets/sprites/asteroid_32x32x55.png');
+		this.load.image('asteroid', 'assets/sprites/asteroid.png');
 
+		// Projectiles
+		this.load.image('bullet', 'assets/sprites/bullet.png');
+		this.load.image('bullet-2', 'assets/sprites/enemy-bullet.png');
+
+		// Misc
 		this.load.image('steel', 'assets/sprites/steel-1.png');
 
+		// Powerups
 		this.load.image('orb', 'assets/sprites/orb.png');
 		this.load.image('orb-red', 'assets/sprites/orb-red.png');
 		this.load.image('orb-green', 'assets/sprites/orb-green.png');
@@ -31,43 +54,49 @@ class BootScene extends Phaser.Scene
 		this.load.image('flame', 'assets/sprites/flame.png');
 		this.load.image('ice', 'assets/sprites/diamond.png');
 
+		return this;
+	}
+
+	preloadFonts()
+	{
 		this.load.image('font', 'assets/sprites/ulm.png');
 
+		return this;
+	}
+
+	preloadSpritesheets()
+	{
 		this.load.spritesheet('explode', 'assets/sprites/boom32wh12.png', {
 			frameWidth: 32,
 			frameHeight: 32,
 			endFrame: 11
 		});
 
-		// TBA
-		// this.load.image('asteroids', 'assets/sprites/asteroid_32x32x55.png');
-		this.load.image('asteroid', 'assets/sprites/asteroid.png');
+		return this;
+	}
 
+	preloadAudio()
+	{
 		this.load.audio('sfx', [
 			'assets/audio/mixdown.ogg',
 			'assets/audio/mixdown.mp3'
 		]);
 
-		let progress = this.add.graphics();
-
-		this.load.on('progress', value =>
-		{
-			progress.clear();
-			progress.fillStyle(0xffffff, 1);
-			progress.fillRect(0, this.sys.game.config.height / 2, this.sys.game.config.width * value, 60);
-		});
-
-		this.load.on('complete', () =>
-		{
-			progress.destroy();
-
-			this.scene.start('GameScene');
-		});
+		return this;
 	}
 
-	create()
+	onProgress(value)
 	{
-		// this.scene.start('GameScene');
+		this.progress.clear();
+		this.progress.fillStyle(0xffffff, 1);
+		this.progress.fillRect(0, this.sys.game.config.height / 2, this.sys.game.config.width * value, 60);
+	}
+
+	onComplete()
+	{
+		this.progress.destroy();
+
+		this.scene.start('GameScene');
 	}
 
 }
